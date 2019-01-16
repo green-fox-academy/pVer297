5411881740721a70bcb412ae6d33fcb0fa17bc39
Zoltan Egri <egri.zoltan.m@gmail.com>
2019-01-16 14:06:35 +0100

    done Hello-Others

---
+++ week-01/day-3/03-Hello-Others/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+
+    // Greet 3 of your classmates with this program, in three separate lines
+    // like:
+    //
+    // Hello, Esther!
+    // Hello, Mary!
+    // Hello, Joe!
+    std::cout << "Hello, Ádám" << std::endl;
+    std::cout << "Hello, Peti" << std::endl;
+    std::cout << "Hello, Boró";
+
+
+    return 0;
+}
