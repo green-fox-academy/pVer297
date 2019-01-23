652c849210190b5b7f5aabc8b667bc21124eb754
Zoltan Egri <egri.zoltan.m@gmail.com>
2019-01-23 14:36:37 +0100

    refactor divide_by_zero with function

--- week-02/day-3/Divide_by_Zero/main.cpp
+++ week-02/day-3/Divide_by_Zero/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 #include <fstream>
 
+
+double divBy(double input){
+    try {
+        if (!(input < 0 || input > 0)) {
+            throw 2;
+        }
+        return 10.0 / input;
+    } catch (...) {
+        std::cout << "You can't divide with that!"  << std::endl;
+        return 0;
+    }
+}
+
 int main()
 {
     // Create a function that takes a number
@@ -8,19 +21,10 @@ int main()
     // and prints the result.
     // It should print "fail" if the parameter is 0
     std::cout << "Please provide me a number:" << std::endl;
-    int userInput;
-    double result = 0;
+    double userInput;
     std::cin >> userInput;
 
-    try {
-        if (!(userInput < 0 || userInput > 0)) {
-            throw 2;
-        }
-        result = 10.0 / userInput;
-        std::cout << "The result is: " << result << std::endl;
-    } catch (...) {
-        std::cout << "You can't divide with that!"  << std::endl;
-    }
+    std::cout << divBy(userInput) << std::endl;
 
     return 0;
 }
