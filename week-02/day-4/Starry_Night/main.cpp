9cd5a5f53aa60718b30fe82ad8d2199d2d8fadbc
Zoltan Egri <egri.zoltan.m@gmail.com>
2019-01-24 21:55:29 +0100

    done starry_night

---
+++ week-02/day-4/Starry_Night/main.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+#include <SDL.h>
+#include <ctime>
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+const unsigned int RAND_SEED = time(0);
+
+//Draws geometry on the canvas
+void draw();
+
+//Starts up SDL and creates window
+bool init();
+
+//Frees media and shuts down SDL
+void close();
+
+//The window we'll be rendering to
+SDL_Window *gWindow = nullptr;
+
+//The window renderer
+SDL_Renderer *gRenderer = nullptr;
+void drawStar();
+
+
+void draw() {
+    // Draw the night sky:
+    //  - The background should be black
+    //  - The stars can be small squares
+    //  - The stars should have random positions on the canvas
+    //  - The stars should have random color (some shade of grey)
+    //
+    // You might have to make modifications somewhere else to create a black background ;)
+    srand(RAND_SEED);
+    for(int i = 0; i < 1000;i++){
+        drawStar();
+    }
+
+}
+
+void drawStar() {
+    int starSize = (rand() % 4) + 1;
+    int starColor = rand() % 255;
+    SDL_Rect star = {rand() % SCREEN_WIDTH, rand() % SCREEN_HEIGHT, starSize, starSize};
+
+    SDL_SetRenderDrawColor(gRenderer, starColor, starColor, starColor, 255);
+    SDL_RenderFillRect(gRenderer, &star);
+
+
+}
+
+bool init() {
+    //Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cout << "SDL could not initialize! SDL Error: " << SDL_GetError() << std::endl;
+        return false;
+    }
+
+    //Create window
+    gWindow = SDL_CreateWindow("Starry night", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+                               SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (gWindow == nullptr) {
+        std::cout << "Window could not be created! SDL Error: " << SDL_GetError() << std::endl;
+        return false;
+    }
+
+    //Create renderer for window
+    gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+    if (gRenderer == nullptr) {
+        std::cout << "Renderer could not be created! SDL Error: " << SDL_GetError() << std::endl;
+        return false;
+    }
+
+    //Initialize renderer color
+    SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+    return true;
+}
+
+void close() {
+    //Destroy window
+    SDL_DestroyRenderer(gRenderer);
+    SDL_DestroyWindow(gWindow);
+    gWindow = nullptr;
+    gRenderer = nullptr;
+
+    SDL_Quit();
+}
+
+int main(int argc, char *args[]) {
+    //Start up SDL and create window
+    if (!init()) {
+        std::cout << "Failed to initialize!" << std::endl;
+        close();
+        return -1;
+    }
+
+    //Main loop flag
+    bool quit = false;
+
+    //Event handler
+    SDL_Event e;
+
+    //While application is running
+    while (!quit) {
+        //Handle events on queue
+        while (SDL_PollEvent(&e) != 0) {
+            //User requests quit
+            if (e.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+
+        //Clear screen
+        SDL_SetRenderDrawColor(gRenderer, 0, 0, 15, 255);
+        SDL_RenderClear(gRenderer);
+
+        draw();
+
+        //Update screen
+        SDL_RenderPresent(gRenderer);
+    }
+
+    //Free resources and close SDL
+    close();
+
+    return 0;
+}
