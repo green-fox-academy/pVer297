4d0a1c73f16d47b4913b452c9f0040803fc5fff4
Zoltan Egri <egri.zoltan.m@gmail.com>
2019-03-20 09:36:51 +0100

    print char?

---
+++ week-09/day-03-Practice/Print_Char/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// Modifiy this program to print out the number we want
+
+/* What? */
+
+int main()
+{
+    //printf("%c\n", 65);
+
+    printf("%d\n", 65);
+
+    return 0;
+}
